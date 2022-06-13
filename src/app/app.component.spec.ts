9c7fa775a6aa0095ebcae49d8df18075d86fdfb9
romain-petit39 <45284892+romain-petit39@users.noreply.github.com>
2022-06-13 19:52:59 +0200

    feature: Test FizzBuzz

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -10,6 +10,7 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
+
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
@@ -22,10 +23,57 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('FizzBuzz');
   });
 
-  it('should render title', () => {
+
+  it('should evaluate fizzBuzz(1) return 1', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('FizzBuzz app is running!');
-  });
+    const app= fixture.componentInstance;
+    expect(app.fizzBuzz(1)).toEqual('1');
+  })
+
+  it('should evaluate fizzBuzz(2) return 2', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app= fixture.componentInstance;
+    expect(app.fizzBuzz(2)).toEqual('2');
+  })
+
+  
+  it('should evaluate fizzBuzz(3) return Fizz', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app= fixture.componentInstance;
+    expect(app.fizzBuzz(3)).toEqual('Fizz');
+  })
+
+  it('should evaluate fizzBuzz(5) return Buzz', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app= fixture.componentInstance;
+    expect(app.fizzBuzz(5)).toEqual('Buzz');
+  })
+
+  it('should evaluate fizzBuzz(10) return Buzz', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app= fixture.componentInstance;
+    expect(app.fizzBuzz(10)).toEqual('Buzz');
+  })
+
+  it('should evaluate fizzBuzz(15) return FizzBuzz', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app= fixture.componentInstance;
+    expect(app.fizzBuzz(15)).toEqual('FizzBuzz');
+  })
+
+  it('should evaluate fizzBuzz(30) return FizzBuzz', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app= fixture.componentInstance;
+    expect(app.fizzBuzz(30)).toEqual('FizzBuzz');
+  })
+
+  it('should evaluate fizzBuzz(100) return FizzBuzz', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app= fixture.componentInstance;
+    expect(app.fizzBuzz(100)).toEqual('Buzz');
+  })
+
+
+
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'FizzBuzz';
+  fizzBuzzResponses: String[] = [];
+
+  ngOnInit(): void {
+    for (let i = 1; i <= 100; i++ ) {
+      this.fizzBuzzResponses.push(this.fizzBuzz(i));
+    }
+    
+  }
+
+  fizzBuzz(number: number) {
+    if (number % 5 === 0 && number % 3 === 0) {
+      return 'FizzBuzz';
+    } else if (number % 3 === 0) {
+      return 'Fizz';
+    } else if (number % 5 === 0) {
+      return 'Buzz';
+    } else {
+      return number.toString();
+    }
+  }
 }
